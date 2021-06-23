c5fa7d4aaaff18e56699f59c9658de9451373b9a
Tudor Nicoara <tudornicoara@yahoo.com>
2021-06-23 22:58:27 +0300

    Angular 11

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -24,10 +24,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ]),
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'counter', component: CounterComponent },
+    { path: 'fetch-data', component: FetchDataComponent },
+], { relativeLinkResolution: 'legacy' }),
     BrowserAnimationsModule
   ],
   providers: [],
